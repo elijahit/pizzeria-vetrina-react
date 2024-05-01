bc7a054a05e7ec226a2b1bff45b058d241cb6ffe
Elijah <gabriele.tosto@outlook.com>
2024-05-01 20:40:42 +0200

    ADD: Aggiunto scale allo staff section

--- src/components/StaffSection.jsx
+++ src/components/StaffSection.jsx
@@ -35,16 +35,20 @@ function StaffSection() {
   setTimeout(() => {
     setDivOpacity("opacity-no opacity-timing");
   }, 100);
-  
-  return(
+
+  return (
     <>
-      <div className="bg-personality p-4"> 
+      <div className="bg-personality p-4">
         <div className={"container mb-5 " + divOpacity}>
           <h2 className="text-center mb-4 text-white">Il nostro staff</h2>
           <div className="row">
             <div className="ms-auto col-12 d-flex flex-column flex-lg-row flex-wrap gap-5 justify-content-center align-items-center text-white">
               {StaffList.map((list) => {
-                return <StaffCard staffName={list.name} staffPosition={list.position} staffExperience={list.experience} staffIMG={list.imgUrl} key={Math.random()} />;
+                return (
+                  <div className="staff-animation">
+                    <StaffCard staffName={list.name} staffPosition={list.position} staffExperience={list.experience} staffIMG={list.imgUrl} key={Math.random()} />
+                  </div>
+                );
               })}
             </div>
           </div>
