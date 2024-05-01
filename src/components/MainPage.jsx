f3ee57ed33708031e90ff9bad53b667d2c78c27f
Elijah <gabriele.tosto@outlook.com>
2024-05-01 20:00:47 +0200

    ADD: Aggiunta delivery section

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,11 +2,13 @@ import "./MainPage.css";
 import HeroSection from "./HeroSection";
 import HourSection from "./HourSection";
 import StaffSection from "./StaffSection";
+import DeliverySection from "./DeliverySection";
 
 function Main() {
   return (
     <main>
       <HeroSection />
+      <DeliverySection />
       <StaffSection />
       <HourSection />
     </main>
